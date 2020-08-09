1df0c0b0575bca8cee470601b32bebb53b412108
sreexamus <sreestarta@gmail.com>
2020-08-09 18:19:27 -0400

    Conditional Compiling
    Sample Proj to explain conditional compiling

---
+++ ActiveCompilation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ActiveCompilation
+//
+//  Created by sreekanth reddy iragam reddy on 8/9/20.
+//  Copyright © 2020 com.test.Sample. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
